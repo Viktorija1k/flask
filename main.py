7e79776ed6191b9848e801436c6dd3ec8ae6b26e
Viktorija1k <vika4a5@inbox.lv>
2024-06-28 18:04:29 +0000

    28/06/24

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html') 
@@ -12,7 +11,7 @@ def par_mums():
     return render_template('par_mums.html')
 
 @app.route('/kontakti')
-def koontakti():
+def kontakti():
     return render_template('kontakti.html')
 
 @app.route('/sveiciens')
@@ -29,10 +28,10 @@ def mainigie():
     vecums = 20	
     vecums2 = vecums + vecums
     return render_template('mainigie.html', vards = vards, vecums = vecums,
-                           vecums2 = vecums2)
+    vecums2 = vecums2)
 
 @app.route('/datu_tipi')
-def dati_tipi():
+def datu_tipi():
     teksts = "Sveika pasaule!"
     skaitlis = 10
     decimals = 10.5
@@ -43,46 +42,108 @@ def dati_tipi():
                            decimals=decimals, saraksts=saraksts, mans_dict=mans_dict,
                           mans_kopa=mans_kopa)
 
+@app.route('/operatori')
+def operatori():
+    a = 10
+    b = 3
+    summa =  a + b
+    starpiba = a - b
+    reizinajums = a * b
+    dalijums = a / b
+    atlikums = a % b
+    vienads = (a == b)
+    return render_template('operatori.html', summa=summa, starpiba=starpiba,
+                           reizinajums=reizinajums,dalijums=dalijums, atlikums=atlikums,
+                           vienads=vienads)
+
 @app.route('/kontroles_strukturas')
 def kontroles_strukturas():
     x = 3
     if x > 5:
-        rezultats = "x ir lielāks par 5"
+         rezultats = "x ir lielāks par 5"
     else:
         rezultats = "x ir mazāks par 5"
-    for_cikla_rezultats =[i for i in range(6,31)]
+    for_cikla_rezultats = [i for i in range(6, 31)]
     while_cikla_rezultats = []
     y = 0
     while y < 10:
         while_cikla_rezultats.append(y)
         y += 1
-        
     return render_template('kontroles_strukturas.html', rezultats=rezultats, 
-                           for_cikla_rezultats = for_cikla_rezultats,
-                           while_cikla_rezultats = while_cikla_rezultats)
+                                   for_cikla_rezultats=for_cikla_rezultats, 
+                                   while_cikla_rezultats=while_cikla_rezultats)
 
 
 @app.route('/funkcijas')
 def funkcijas():
     def sveiciens(vards="Jānis"):
         return f"Sveiki! {vards}!"
-    sveiciens = sveiciens()
-    
-    return render_template('funkcijas.html', sveiciens=sveiciens)
+    sveiciens1 = sveiciens()
+    sveiciens2 = sveiciens("Irina")
+    sveiciens3 = sveiciens1 + sveiciens2
+    return render_template('funkcijas.html', sveiciens1=sveiciens1, 
+                           sveiciens2=sveiciens2, sveiciens3=sveiciens3)
 
 
 @app.route('/failu_apstrade')
-def faila_apstrade():
+def failu_apstrade():
     saturs= ""
     with open("teksts.txt", "r") as fails:
         saturs = fails.read()
     return render_template('failu_apstrade.html', saturs=saturs)
 
+@app.route('/moduli')
+def moduli():
+    import math
+    sqrt_rezultats = math.sqrt(16)
+    return render_template('moduli.html', sqrt_rezultats=sqrt_rezultats)
 
-@app.route('/aiziet' , methods=['POST'])
+@app.route('/oop')
+def oop():
+    class Persona:
+        def __init__(self, vards, vecums):
+            self.vards = vards
+            self.vecums = vecums
+            
+        def sveiciens(self):
+            return f"Sveiki, mani sauc {self.vards} un man ir {self.vecums} gadi."
+    parsona1 = Persona("Jānis", 25)
+    sveiciens = parsona1.sveiciens()
+    
+    return render_template('oop.html', sveiciens=sveiciens)
+        
+
+
+
+@app.route('/aiziet', methods=['POST'])
 def aiziet():
     lietotajs = request.form['lietotajvards']
     return f"Paldies, {lietotajs}! Jūsu pieteikums ir saņemts."
+
+@app.route('/majasdarbs')
+def majasdarbs():
+    return render_template('majasdarbs.html')
+
+@app.route('/majasdarbs2', methods=['POST'])
+def majasdarbs2():
+    a = int(request.form['a'])
+    b = int(request.form['b'])
+    if a == b:
+        rez = "Abi skaitli ir vienadi"
+    elif a > b:
+        rez = "Pirmais skaitlis ir lielāks"
+    else:
+        rez = "Otrais skaitlis ir lielāks"
+    return render_template('majasdarbs.html', rez=rez)
+
+@app.route('/aptauja')
+def aptauja():
+    return render_template('aptauja.html')
+
+@app.route('/pieteikties')
+def pieteikties():
+    return render_template('pieteikties.html')
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
